38f965425a2a9224f0a35bffeba6330caa2baa28
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-16 15:17:23 +0300

    ADDS CODE IN THE INIT.PY FILE

--- config.py
+++ config.py
@@ -2,5 +2,14 @@ class Config:
     pass
 
 
+class DevConfig:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevson:Antidolofinomonoligasta102@localhost/phoenixdb'
+
+
 class ProdConfig(Config):
     pass
+
+config_options = {
+    'development' : DevConfig,
+    'production': ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY='Kevson102'
+
+python manage.py server
